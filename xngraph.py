e3f9b0400acd1751c74a89dc8ffb5a1a3cb5f2a1
Steve Kerrison <steve@stevekerrison.com>
2014-01-27 13:01:42 +0000

    Objectify for use in some other tools of mine that don't use dot/graphviz directly

--- xngraph.py
+++ xngraph.py
@@ -44,58 +44,76 @@ from docopt import docopt
 import xml.etree.ElementTree as ET
 import re
 
-# XMOS XN file namespace
-NS = 'http://www.xmos.com'
+class XNReader:
+  # XMOS XN file namespace
+  NS = 'http://www.xmos.com'
 
-def FreqConv(value):
-  """Convert frequencies with (M|K)Hz in them to floats"""
-  conv = { 'm' : 1e6,
-    'k' : 1e3 }
-  m = re.match(r"([0-9\.]+)\s*(.)?(hz)?",str(value).lower())
-  ret = float(m.group(1))
-  if m.group(2) in conv:
-    ret *= conv[m.group(2)]
-  return ret
+  def __init__(self,XNFilename,ignorenodes=[]):
+    t = ET.parse(XNFilename)
+    self.nodes = {}
+    self.links = []
+    if t.getroot().tag != '{{{}}}Network'.format(XNReader.NS):
+      raise Exception('Invalid namespace, should be {}'.format(XNReader.NS))
+    for Node in [ x for x in t.iter('{{{}}}Node'.format(XNReader.NS))
+      if x.attrib['Id'] not in ign ]:
+      freqs = { 'Oscillator': 20e6,
+        'SystemFrequency': 400e6,
+        'ReferenceFrequency': 100e6 }
+      freqs.update( [(x,self.FreqConv(Node.attrib[x]))
+        for x in freqs.keys() if x in Node.attrib] )
+      self.nodes[Node.attrib['Id']] = freqs
+    for Link in t.iter('{{{}}}Link'.format(XNReader.NS)):
+      if 'Flags' in Link.attrib and Link.attrib['Flags'] in ["SOD","XSCOPE"]:
+        continue #Skip XScope!
+      encoding = Link.attrib['Encoding'][0]
+      if 'Delays' in Link.attrib:
+        delays = Link.attrib['Delays']
+      Ends = Link.findall('{{{}}}LinkEndpoint'.format(XNReader.NS))
+      if any(map(lambda(x): x.attrib['NodeId'] in ign, Ends)):
+        continue #Skip links involving ignored nodes
+      if 'Delays' in Ends[0].attrib:
+        ldelay = Ends[0].attrib['Delays']
+      else:
+        ldelay = delays
+      self.links.append({'src':Ends[0].attrib['NodeId'],
+        'dst': Ends[1].attrib['NodeId'],
+        'enc': encoding,
+        'del': ldelay})
+      if 'Delays' in Ends[1].attrib:
+        ldelay = Ends[1].attrib['Delays']
+      else:
+        ldelay = delays
+      self.links.append({'src':Ends[1].attrib['NodeId'],
+        'dst': Ends[0].attrib['NodeId'],
+        'enc': encoding,
+        'del': ldelay})
+  
+  def FreqConv(self,value):
+    """Convert frequencies with (M|K)Hz in them to floats"""
+    conv = { 'm' : 1e6,
+      'k' : 1e3 }
+    m = re.match(r"([0-9\.]+)\s*(.)?(hz)?",str(value).lower())
+    ret = float(m.group(1))
+    if m.group(2) in conv:
+      ret *= conv[m.group(2)]
+    return ret
 
 if __name__ == '__main__':
   arguments = docopt(__doc__)
-  t = ET.parse(arguments['<xnfile>'])
-  if t.getroot().tag != '{{{}}}Network'.format(NS):
-    raise Exception('Invalid namespace, should be {}'.format(NS))
   ign = []
   if arguments['--ignore-nodes']:
     ign = arguments['--ignore-nodes'].split(',')
-
+  XNR = XNReader(arguments['<xnfile>'],ign)
   # Initial DOT setup
   print 'digraph {} {{'.format(''.join(arguments['<xnfile>'].split('.')[:-1]))
   print '  rankdir=LR'
-
-  #Now declare node names and frequencies
-  for Node in [ x for x in t.iter('{{{}}}Node'.format(NS))
-    if x.attrib['Id'] not in ign ]:
-    freqs = { 'Oscillator': 20e6,
-      'SystemFrequency': 400e6,
-      'ReferenceFrequency': 100e6 }
-    freqs.update( [(x,FreqConv(Node.attrib[x]))
-      for x in freqs.keys() if x in Node.attrib] )
+  for n,a in XNR.nodes.items():
     print """  "{0}" [
     shape = "record"
     label = "{0} | {1:e} / {2:e} / {3:e}"
-  ];""".format(Node.attrib['Id'], freqs['Oscillator'],
-      freqs['ReferenceFrequency'], freqs['SystemFrequency'])
-
-  # Find and declare relevant links and widths/delays 
-  for Link in t.iter('{{{}}}Link'.format(NS)):
-    if 'Flags' in Link.attrib and Link.attrib['Flags'] == 'SOD':
-      continue #Skip XScope!
-    l = 'label="{},{}"'.format( Link.attrib['Encoding'][0],
-      Link.attrib['Delays'] )
-    Ends = Link.findall('{{{}}}LinkEndpoint'.format(NS))
-    if any(map(lambda(x): x.attrib['NodeId'] in ign, Ends)):
-      continue #Skip links involving ignored nodes
-    print '  {} -> {} [{}];'.format( Ends[0].attrib['NodeId'],
-      Ends[1].attrib['NodeId'], l )
-    print '  {} -> {} [{}];'.format( Ends[1].attrib['NodeId'],
-      Ends[0].attrib['NodeId'], l ) 
-      
+  ];
+    """.format(n,a['Oscillator'],a['ReferenceFrequency'],a['SystemFrequency'])
+  for l in XNR.links:
+    print """  {} -> {} [label="{},{}"];""".format(l['src'],l['dst'], l['enc'],
+      l['del'])
   print '}'
